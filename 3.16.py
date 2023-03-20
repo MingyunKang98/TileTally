ed5526b521de0cc719238ce6d027b5a763f0578b
junsang075 <junsang075@gmail.com>
2023-03-20 17:03:06 +0900

    junsang

--- 3.16.py
+++ 3.16.py
@@ -225,3 +225,8 @@ def show_result() :
     plt.show()
     plt.close()
 
+cv2.namedWindow('img')
+cv2.setMouseCallback('img',mouse_handler)
+cv2.imshow('img',src_img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
