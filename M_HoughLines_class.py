2a4daf3a6b8c722493d4bf64e2d8859e026fc50d
MingyunKang <kangmingyun@gangmingyuns-MacBook-Pro.local>
2023-03-22 15:07:52 +0900

    class modified

---
+++ M_HoughLines_class.py
@@ -0,0 +1,42 @@
+import cv2
+import matplotlib.pyplot as plt
+
+class InteractivePlot:
+    def __init__(self, img_dir):
+        self.img = cv2.imread(img_dir)
+        self.fig, self.ax = plt.subplots(figsize=(15, 8))
+        self.xdata = []
+        self.ydata = []
+        self.line, = self.ax.plot(self.xdata, self.ydata)
+        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.add_point)
+        self.ax.set_aspect('auto', adjustable='box')
+        plt.imshow(cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB))
+
+    def add_point(self, event):
+        if event.inaxes != self.ax:
+            return
+        if event.button == 1:
+            x = event.xdata
+            y = event.ydata
+            self.xdata.append(x)
+            self.ydata.append(y)
+            plt.scatter(self.xdata, self.ydata)
+            self.line.set_data(self.xdata, self.ydata)
+            plt.draw()
+        if event.button == 3:
+            self.xdata.pop()
+            self.ydata.pop()
+            self.line.set_data(self.xdata, self.ydata)
+            plt.draw()
+        if event.button == 2:
+            self.fig.canvas.mpl_disconnect(self.cid)
+            plt.close()
+
+    def show(self):
+        plt.show()
+
+if __name__ == "__main__":
+    dir = "./Base01.jpg"
+    plot = InteractivePlot(dir)
+    plot.show()
+    print(plot.xdata)
