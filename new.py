408403ff6348afdcb95c9d24402ee49214c50b0d
junsang075 <junsang075@gmail.com>
2023-03-24 00:51:56 +0900

    angle tile 맞추기 전

--- new.py
+++ new.py
@@ -315,7 +315,7 @@ elif aver_y[0] < 10 and (height-aver_y[-1]) > 10 : # 아래만 있을경우
         ud_btile = round((num_vertical - 1) / 2)
 
 if aver_y[0] > 10 and (height-aver_y[-1]) > 10 :    # 둘 다 있을경우
-    if  aver_y[0] > aver_h / 2 and (height-aver_y[-1]) > aver_h / 2:
+    if  (aver_y[0] > aver_h / 2 and (height-aver_y[-1]) > aver_h / 2) or aver_y[0] + (height-aver_y[-1]) > aver_h+5:
         ud_btile = (num_vertical-1)*2
     else :  #aver_y[0] > aver_h / 2 and (height-aver_y[-1]) < aver_h / 2:
         ud_btile = (num_vertical - 1)
@@ -324,20 +324,20 @@ if aver_y[0] > 10 and (height-aver_y[-1]) > 10 :    # 둘 다 있을경우
 if aver_x[0] < 10 and (width-aver_x[-1]) < 10 :    # 왼쪽줄, 오른쪽줄 에 쪼개진 타일 없는경우
     rl_btile = 0
 
-elif aver_x[0] > 10 and (width-aver_x[-1]) < 10 :   # 위쪽만 있을경우
+elif aver_x[0] > 10 and (width-aver_x[-1]) < 10 :   # 왼쪽만 있을경우
     if  aver_x[0] > aver_w / 2 :
         rl_btile = num_horizon -1
     else:
         rl_btile = round((num_horizon -1)/2)
 
-elif aver_x[0] < 10 and (width-aver_x[-1]) > 10 : # 아래만 있을경우
+elif aver_x[0] < 10 and (width-aver_x[-1]) > 10 : # 오른쪽만 있을경우
     if (width-aver_x[-1]) > aver_w / 2:
         rl_btile = num_horizon - 1
     else:
         rl_btile = round((num_horizon - 1) / 2)
 
 if aver_x[0] > 10 and (width-aver_x[-1]) > 10 :    # 둘 다 있을경우
-    if  aver_x[0] > aver_w / 2 and (width-aver_x[-1]) > aver_w / 2:
+    if  (aver_x[0] > aver_w / 2 and (width-aver_x[-1]) > aver_w / 2) or aver_x[0] + (width-aver_x[-1]) > aver_w+5:
         rl_btile = (num_horizon-1)*2
     else :  #aver_y[0] > aver_h / 2 and (height-aver_y[-1]) < aver_h / 2:
         rl_btile = (num_horizon - 1)
